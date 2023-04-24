5272f118f2d3e0fa28a457f7ed7bc3e5158f1e58
vincenttommi <vincenttommi@gmail.com>
2023-04-24 05:01:33 +0300

    list and keys

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,15 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{/* display an <a> tag for each link here */
+  links.map(link =>{
+    const navi = `#${link}`
+    // console.log(navi)
+    return(
+      <a key={link} href={navi} > {link}</a>
+    )
+  })
+  }</nav>;
 }
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,16 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{  
+      projects.map(proj =>{
+        const {id, name, about, technologies } = proj
+
+        return(
+        <ProjectItem key={id} name={name} about = {about} technologies={technologies} />);
+      })      
+      /* render ProjectItem components here */
+      }
+      </div>
     </div>
   );
 }
